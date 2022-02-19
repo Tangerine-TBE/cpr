2f57d2a9b14d1967f0cfdd8a6b573e895abccc3b
hunger719 <2302156238@qq.com>
2022-02-19 17:20:44 +0800

    事件页面状态调整

--- app/src/main/java/com/pr/perfectrecovery/fragment/CheckEventFragment.kt
+++ app/src/main/java/com/pr/perfectrecovery/fragment/CheckEventFragment.kt
@@ -1,13 +1,21 @@
 package com.pr.perfectrecovery.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.pr.perfectrecovery.base.BaseConstant
+import com.pr.perfectrecovery.bean.MessageEventData
 import com.pr.perfectrecovery.databinding.EventFragmentBinding
 import com.pr.perfectrecovery.fragment.viewmodel.EventViewModel
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class CheckEventFragment : Fragment() {
     private lateinit var viewBinding: EventFragmentBinding
@@ -28,11 +36,20 @@ class CheckEventFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        EventBus.getDefault().register(this)
         viewModel = ViewModelProvider(this).get(EventViewModel::class.java)
         initVIew()
     }
 
     private fun initVIew() {
+        viewBinding.root.children.forEach {
+            if (it is CheckBox) {
+                it.isEnabled = false
+                it.setTextColor(Color.GRAY)
+
+            }
+        }
+
         viewBinding.checkBox.setOnCheckedChangeListener { compoundButton, b ->
 
         }
@@ -64,6 +81,19 @@ class CheckEventFragment : Fragment() {
 
         }
 
+    }
+
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public fun onEvent(event: MessageEventData) {
+        if (event.code == BaseConstant.EVENT_SINGLE_CHART_START) {
+            viewBinding.root.children.forEach {
+                if (it is CheckBox) {
+                    it.isEnabled = true
+                    it.setTextColor(Color.WHITE)
+
+                }
+            }
+        }
     }
 
 }
